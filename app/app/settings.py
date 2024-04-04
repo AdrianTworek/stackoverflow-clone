79aafe0aef39c75232fa78dd30287e17e0c4e212
Adrian Tworek <adrian.tworek.it@gmail.com>
2024-04-05 01:49:42 +0200

    chore: install and setup bootstrap 5 and crispy forms

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'crispy_forms',
+    'crispy_bootstrap5',
+
     'main',
     'users',
 ]
@@ -69,6 +72,10 @@ TEMPLATES = [
         },
     },
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 WSGI_APPLICATION = 'app.wsgi.application'
 
