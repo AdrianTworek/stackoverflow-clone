452a0ebd9df2d51c3e18e74a41ed0af0053ca614
Adrian Tworek <adrian.tworek.it@gmail.com>
2024-04-05 01:52:24 +0200

    feat: register and login

--- app/users/views.py
+++ app/users/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import AuthenticationForm
+from . import forms
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = forms.RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('main:home')
+    else:
+        form = forms.RegisterForm()
+
+    return render(request, 'users/register.html', {'form': form})
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('main:home')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'users/login.html', {'form': form})
