fd091156efb5c774a6d26a8bded1b7ba8a0540d1
Adrian Tworek <adrian.tworek.it@gmail.com>
2024-04-04 21:55:32 +0200

    refactor: move base templates and views to separate main app

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', include('main.urls'))
 ]
