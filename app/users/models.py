0fc1095264df6ecb7f69741dbf16d184019e5cb4
Adrian Tworek <adrian.tworek.it@gmail.com>
2024-04-08 18:05:00 +0200

    feat: add abstract and profile models

--- app/users/models.py
+++ app/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from main.models import AbstractModel
 
-# Create your models here.
+
+class Profile(AbstractModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    image = models.ImageField(
+        default='default_user_image.jpg', upload_to='profile_images')
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
