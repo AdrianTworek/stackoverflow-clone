405deb075e00c00a5c908a2dbba9aaed0e749755
Adrian Tworek <adrian.tworek.it@gmail.com>
2024-04-08 17:40:30 +0200

    chore: install pillow and setup media

--- app/app/settings.py
+++ app/app/settings.py
@@ -51,6 +51,7 @@ SOCIALACCOUNT_PROVIDERS = {
     }
 }
 
+LOGIN_URL = '/users/login'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/users/login'
 
@@ -157,6 +158,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
