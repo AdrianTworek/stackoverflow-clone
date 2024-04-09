1fe6d8a83389e5c26cb1b36765fe887ebb2a9c55
Adrian Tworek <adrian.tworek.it@gmail.com>
2024-04-09 22:54:02 +0200

    feat: update profile view and template

--- app/users/urls.py
+++ app/users/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('profile/', views.profile, name='profile'),
+    path('profile/update/', views.profile_update, name='profile_update')
 ]
--- app/users/views.py
+++ app/users/views.py
@@ -2,21 +2,21 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login, logout as auth_logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
-from . import forms
+from .forms import RegisterForm, UserUpdateForm, ProfileUpdateForm
 from main.decorators import anonymous_required
 
 
 @anonymous_required(redirect_url='main:home')
 def register(request):
     if request.method == 'POST':
-        form = forms.RegisterForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
             auth_login(request, user,
                        backend='django.contrib.auth.backends.ModelBackend')
             return redirect('main:home')
     else:
-        form = forms.RegisterForm()
+        form = RegisterForm()
 
     return render(request, 'users/register.html', {'form': form})
 
@@ -44,3 +44,25 @@ def logout(request):
 @login_required()
 def profile(request):
     return render(request, 'users/profile.html')
+
+
+@login_required()
+def profile_update(request):
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return redirect('main:users:profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form
+    }
+
+    return render(request, 'users/profile_update.html', context)
