56bb681776698d4c025bf8ed5584296688fafb0f
Adrian Tworek <adrian.tworek.it@gmail.com>
2024-04-09 22:54:58 +0200

    feat: add image resizing to Profile model using pillow

--- app/users/models.py
+++ app/users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from main.models import AbstractModel
+from PIL import Image
 
 
 class Profile(AbstractModel):
@@ -11,3 +12,11 @@ class Profile(AbstractModel):
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
