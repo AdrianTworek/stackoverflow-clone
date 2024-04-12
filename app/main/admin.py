7d50cb4d888a12a0498d23b44a90e3ad81ef1636
Adrian Tworek <adrian.tworek.it@gmail.com>
2024-04-12 19:12:00 +0200

    feat: Question model

--- app/main/admin.py
+++ app/main/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
 from users.models import Profile
+from questions.models import Question
 
-admin.site.register(Profile)
+admin.site.register([Profile, Question])
--- app/questions/models.py
+++ app/questions/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from main.models import AbstractModel
 
-# Create your models here.
+
+class Question(AbstractModel):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
