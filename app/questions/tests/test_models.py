f6691ba9133c3703fc44dd0940d616d80c38d0c8
Adrian Tworek <adrian.tworek.it@gmail.com>
2024-05-08 19:18:53 +0200

    test: answer model

--- app/questions/tests/test_models.py
+++ app/questions/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from ..models import Tag, Question
+from ..models import Tag, Question, Answer
 
 
 class TagModelTest(TestCase):
@@ -54,3 +54,23 @@ class QuestionModelTest(TestCase):
     def test_get_absolute_url(self):
         question = Question.objects.get(id=1)
         self.assertEqual(question.get_absolute_url(), '/questions/1/')
+
+
+class AnswerModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        User.objects.create_user(username='test', password='test')
+        Question.objects.create(
+            title='Test question', content='Test content', author=User.objects.get(id=1))
+        Answer.objects.create(
+            content='Test answer', question=Question.objects.get(id=1), author=User.objects.get(id=1))
+
+    def test_content_label(self):
+        answer = Answer.objects.get(id=1)
+        content_label = answer._meta.get_field('content').verbose_name
+        self.assertEqual(content_label, 'content')
+
+    def test_object_name_is_question_title(self):
+        answer = Answer.objects.get(id=1)
+        expected_object_name = f"Answer to '{answer.question.title}'"
+        self.assertEqual(expected_object_name, str(answer))
