555b1e5c5b8ed4ba011b551d0db67d6d36d1c7f5
Adrian Tworek <adrian.tworek.it@gmail.com>
2024-05-21 18:47:58 +0200

    test: user profile, user profile update, user questions views

--- app/users/tests/test_views.py
+++ app/users/tests/test_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.auth.models import User
+from questions.models import Question
 
 
 class UserViewsTest(TestCase):
@@ -77,3 +78,63 @@ class UserViewsTest(TestCase):
         self.client.login(username='testuser', password='testpassword')
         response = self.client.post(reverse('main:users:logout'))
         self.assertRedirects(response, reverse('main:users:login'))
+
+    def test_profile_view(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword',
+        )
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('main:users:profile'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/profile.html')
+
+    def test_profile_update_view(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword',
+        )
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('main:users:profile_update'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/profile_update.html')
+
+        form_data = {
+            'username': 'testuser',
+            'email': 'test@test.com',
+        }
+        response = self.client.post(
+            reverse('main:users:profile_update'), data=form_data)
+        self.assertRedirects(response, reverse('main:users:profile'))
+
+    def test_profile_update_view_invalid_data(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword',
+        )
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('main:users:profile_update'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/profile_update.html')
+
+        invalid_form_data = {
+            'username': '',
+            'email': 'test@',
+        }
+        response = self.client.post(
+            reverse('main:users:profile_update'), data=invalid_form_data)
+        self.assertContains(response, 'This field is required.')
+        self.assertContains(response, 'Enter a valid email address.')
+
+    def test_user_questions_view(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword',
+        )
+        self.client.login(username='testuser', password='testpassword')
+        Question.objects.create(title='Test question',
+                                content='Test content', author=self.user)
+        response = self.client.get(reverse('main:users:user_questions'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/profile.html')
+        self.assertEqual(len(response.context['questions']), 1)
