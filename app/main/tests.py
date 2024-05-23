1652a2c97b199d2fdad7d24d54f728fd3bec36c6
Adrian Tworek <adrian.tworek.it@gmail.com>
2024-05-23 18:25:15 +0200

    test: main home view

--- app/main/tests.py
+++ app/main/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class MainViewsTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_home_view(self):
+        response = self.client.get(reverse('main:home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main/home.html')
